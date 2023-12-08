60cd9a6afce5da460996074988addce0f7b32417
Corbin <Corbinchristopher1@gmail.com>
2023-12-08 12:30:55 -0700

    init print function

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+void print();
+
 int main() 
 {
     cout << "Hello World!" << endl;
     return 0;
+}
+
+void print()
+{
+    cout << "Welcome to your bank account!" << endl;
+    cout << "-----------------------------" << endl;
+    cout << "What would you like to do?" << endl;  
+    cout << "1. Check Balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "4. Exit" << endl;
 }
